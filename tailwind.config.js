3177f63c09f8112f922399a2b67463ca4f812d27
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-23 14:09:29 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#181A1D",
+        primary: {
+          1: "#222529",
+          2: "#3B3E44",
+        },
+        text: "#CECFD0",
+        subtext: "#707172",
+        mark: {
+          1: "#FF3968",
+          2: "#A2C100",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
