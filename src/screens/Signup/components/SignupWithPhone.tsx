2976651ca13d25f6e68647a395d16ab30b261706
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-24 19:17:43 +0200

    fix(signup): signup with phone verification

--- src/screens/Signup/components/SignupWithPhone.tsx
+++ src/screens/Signup/components/SignupWithPhone.tsx
@@ -1,58 +1,10 @@
 import React, { useState } from "react";
-import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { auth } from "../../../firebase-config";
 import { MdEmail } from "react-icons/md";
 import { SignupUserWithPhone } from "../../../types/index";
+import { requestOTP, SignupPhone } from "../../../utils";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { E164Number } from "libphonenumber-js/types";
-
-const generateRecaptha = () => {
-  window.recaptchaVerifier = new RecaptchaVerifier(
-    "recaptcha-container",
-    {
-      size: "invisible",
-      callback: (response: any) => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-        console.log("generateRecaptha");
-        console.log(response);
-      },
-    },
-    auth
-  );
-};
-
-const handleSubmit = (
-  e: React.FormEvent<HTMLFormElement>,
-  user: SignupUserWithPhone,
-  phone: E164Number | undefined
-) => {
-  e.preventDefault();
-  generateRecaptha();
-  let appVerifier = window.recaptchaVerifier;
-  const tempPhone = phone ? phone.toString() : "";
-  signInWithPhoneNumber(auth, tempPhone, appVerifier)
-    .then((confirmationResult) => {
-      const otp = prompt("please enter the OTP", " ");
-      console.log("OTP: " + otp);
-      confirmationResult
-        .confirm(`${otp}`)
-        .then((result) => {
-          const user = result.user;
-          console.log(user);
-        })
-        .catch((error) => {
-          // User couldn't sign in (bad verification code?)
-          console.log("Error confirmation: ");
-          console.log(error);
-        });
-    })
-    .catch((error) => {
-      // Error; SMS not sent
-      console.log("Error Signin: ");
-      console.log(error);
-    });
-};
 
 const SignupWithPhone = () => {
   const [user, setUser] = useState<SignupUserWithPhone>({
@@ -60,52 +12,82 @@ const SignupWithPhone = () => {
     role: "normal",
   });
   const [phone, setPhone] = useState<E164Number | undefined>();
+  const [otp, setOtp] = useState("");
+  const [verify, setVerify] = useState(false);
   return (
     <div className=" mx-auto">
-      <div className="bg-primary-1 p-5 w-[300px] text-text rounded-lg">
-        <form
-          onSubmit={(e) => {
-            // SignupPhone(e, user);
-            handleSubmit(e, user, phone);
-          }}
-          className=" space-y-4"
-        >
-          <div className="flex flex-col items-center space-y-2">
-            <label className="text-2xl"> Name </label>
-            <input
-              type="text"
-              className="bg-primary-2 p-2 rounded-lg focus:outline-none w-full"
-              onChange={(e) => {
-                setUser({ ...user, name: e.target.value });
+      {verify || (
+        <>
+          <div className="bg-primary-1 p-5 w-[300px] text-text rounded-lg">
+            <form
+              onSubmit={(e) => {
+                SignupPhone(e, phone, setVerify);
               }}
-              required
-            />
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <label className="text-2xl"> Phone </label>
-            <PhoneInput
-              placeholder="Phone Number"
-              value={phone}
-              onChange={setPhone}
-              className="w-full"
-              required
-            />
-          </div>
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="text-white bg-mark-1 text-xl py-1 px-6 rounded-md mt-3"
+              className=" space-y-5"
             >
-              Sign up
-            </button>
+              <div className="flex flex-col items-center space-y-2">
+                <label className="text-2xl"> Name </label>
+                <input
+                  type="text"
+                  className="bg-primary-2 p-2 rounded-lg focus:outline-none w-full"
+                  onChange={(e) => {
+                    setUser({ ...user, name: e.target.value });
+                  }}
+                  required
+                />
+              </div>
+              <div className="flex flex-col items-center space-y-2">
+                <PhoneInput
+                  placeholder="Phone Number"
+                  value={phone}
+                  onChange={setPhone}
+                  className="w-full"
+                  required
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-white bg-mark-2 text-xl py-1 px-6 rounded-md mt-3 font-semibold"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      <div className="bg-primary-1 flex items-center p-2 rounded-md mt-2 space-x-4 text-text hover:bg-gray-700 transition-all ease-in-out hover:cursor-pointer">
-        <MdEmail size={35} />
-        <span className=" text-2xl pb-2">Sign up with Email</span>
-      </div>
-      <div id="recaptcha-container"></div>
+          <div className="bg-primary-1 flex items-center p-2 rounded-md mt-2 space-x-4 text-text hover:bg-gray-700 transition-all ease-in-out hover:cursor-pointer">
+            <MdEmail size={35} />
+            <span className=" text-2xl pb-2">Sign up with Email</span>
+          </div>
+          <div id="recaptcha-container"></div>
+        </>
+      )}
+
+      {verify && (
+        <div className="bg-primary-1 flex flex-col justify-center items-center mt-5 p-5 w-[300px] text-text rounded-lg space-y-4">
+          <label className=" text-mark-2 py-2 px-3 rounded-md text-sm">
+            We've sent you a verification code to your phone number
+            <span className="font-semibold text-mark-1"> {phone}</span>
+          </label>
+          <input
+            type="text"
+            className="bg-primary-2 p-2 rounded-lg focus:outline-none w-full"
+            onChange={(e) => {
+              setOtp(e.target.value);
+            }}
+            required
+          />
+          <button
+            type="button"
+            className="text-white bg-mark-1 text-xl py-1 px-6 rounded-md mt-3 w-fit"
+            onClick={() => {
+              requestOTP(otp, user);
+            }}
+          >
+            Verify
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 import { auth, db } from "../firebase-config";
+import { E164Number } from "libphonenumber-js/types";
+
 import {
   createUserWithEmailAndPassword,
   signInWithPhoneNumber,
@@ -7,13 +9,17 @@ import {
 import { doc, setDoc } from "firebase/firestore";
 import { SignupUserWithEmail, SignupUserWithPhone } from "../types/index";
 
+/*Start of Signup */
 declare global {
   interface Window {
     recaptchaVerifier: RecaptchaVerifier;
     confirmationResult: any;
+    recaptchaWidgetId: any;
+    grecaptcha: any;
   }
 }
 
+//Signup with email
 export const SignupEmail = async (
   e: React.FormEvent<HTMLFormElement>,
   user: SignupUserWithEmail
@@ -34,3 +40,55 @@ export const SignupEmail = async (
       console.log(error);
     });
 };
+
+//Signup with phone
+const generateRecaptha = () => {
+  window.recaptchaVerifier = new RecaptchaVerifier(
+    "recaptcha-container",
+    {
+      size: "invisible",
+      callback: (response: any) => {},
+    },
+    auth
+  );
+};
+
+export const SignupPhone = (
+  e: React.FormEvent<HTMLFormElement>,
+  phone: E164Number | undefined,
+  setVerify: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  e.preventDefault();
+  generateRecaptha();
+  let appVerifier = window.recaptchaVerifier;
+  const tempPhone = phone ? phone.toString() : "";
+  signInWithPhoneNumber(auth, tempPhone, appVerifier)
+    .then((confirmationResult) => {
+      window.confirmationResult = confirmationResult;
+      setVerify(true);
+    })
+    .catch((error) => {
+      // Error; SMS not sent
+      console.log(error);
+    });
+};
+
+export const requestOTP = (otp: string, user: SignupUserWithPhone) => {
+  window.confirmationResult
+    .confirm(otp)
+    .then((result: any) => {
+      const currentUser = result.user;
+      setDoc(doc(db, "users", currentUser.uid), {
+        name: user.name,
+        email: "",
+        phone: currentUser.phoneNumber,
+        role: user.role,
+      });
+      console.log(currentUser);
+    })
+    .catch((error: any) => {
+      // User couldn't sign in (bad verification code?)
+      console.log(error);
+    });
+};
+/*End of Signup */
