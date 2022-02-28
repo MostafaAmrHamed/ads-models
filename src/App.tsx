e5716efbe9a05462516d9cb82ce2069f8bf64b91
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-28 15:35:27 +0200

    feat(app): routing login via email and phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginWithEmail from "./screens/Login/LoginWithEmail";
+import LoginWithPhone from "./screens/Login/LoginWithPhone";
 import SignupWithEmail from "./screens/Signup/SignupWithEmail";
 import SignupWithPhone from "./screens/Signup/SignupWithPhone";
 
 function App() {
   return (
     <div className="container mx-auto">
-      <div className="h-[75px] border border-white"> Navbar </div>
+      <div className="h-[75px] border border-white text-white"> Navbar </div>
       <Routes>
         <Route path="/signupEmail" element={<SignupWithEmail />} />
         <Route path="/signupPhone" element={<SignupWithPhone />} />
+        <Route path="/loginEmail" element={<LoginWithEmail />} />
+        <Route path="/loginPhone" element={<LoginWithPhone />} />
       </Routes>
     </div>
   );
