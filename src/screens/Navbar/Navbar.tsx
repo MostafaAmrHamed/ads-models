83c5aba70181e7c938cb5c268a15662b43d4b3b5
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-03-03 03:57:34 +0200

    feat(navabr): designed the guest, nomral and admin navbar

--- src/screens/Navbar/Navbar.tsx
+++ src/screens/Navbar/Navbar.tsx
@@ -1,7 +1,81 @@
-import React from "react";
-
+import OutsideClickHandler from "react-outside-click-handler";
+import { BiLogIn, BiLogOut } from "react-icons/bi";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { RiDashboardFill } from "react-icons/ri";
+import { CgMenuGridO } from "react-icons/cg";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 function Navbar() {
-  return <div className="h-[75px] border border-white text-white">Navbar</div>;
+  const role = "admin";
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center md:justify-end md:mr-2 mt-5 gap-5">
+      <Link
+        to="/"
+        className="text-text text-2xl font-medium border-2 border-primary-2 px-3 pb-1 hover:bg-primary-2 rounded-md transition-all ease-in-out"
+      >
+        Home
+      </Link>
+      {role ? (
+        <>
+          {role === "admin" && (
+            <Link
+              to="/dashboard"
+              className="bg-primary-2 text-text flex items-center justify-center gap-3 px-3 rounded-md"
+            >
+              <RiDashboardFill size={25} />
+              <span className="text-2xl font-semibold pb-1">Dashboard</span>
+            </Link>
+          )}
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              setOpen(false);
+            }}
+          >
+            <div className="relative">
+              <span
+                className="bg-primary-2 text-text flex items-center justify-center gap-2 px-2 py-1 rounded-full cursor-pointer"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              >
+                <CgMenuGridO size={22} />
+                <FaUserCircle size={30} className="text-mark-1" />
+              </span>
+              {open && (
+                <ul className="absolute w-fit bg-[#292C31] text-text text-xl mt-3 right-1 p-2 rounded-md">
+                  <li className="text-center font-semibold">Mostaf Amr</li>
+                  <li className="flex items-center gap-2 pr-7 pl-2 pb-1 mt-2 hover:bg-primary-2 cursor-pointer select-none rounded-md">
+                    <BiLogOut size={22} />
+                    <span className="pb-1">Logout</span>
+                  </li>
+                </ul>
+              )}
+            </div>
+          </OutsideClickHandler>
+        </>
+      ) : (
+        //Guest navabr
+        <>
+          <Link
+            to="/loginEmail"
+            className="bg-primary-2 text-text flex items-center justify-center gap-3 px-3 rounded-md"
+          >
+            <BiLogIn size={25} />
+            <span className="text-2xl font-semibold pb-1">Login</span>
+          </Link>
+          <Link
+            to="/signupEmail"
+            className="bg-primary-2 text-text flex items-center justify-center gap-3 px-3 rounded-md"
+          >
+            <AiOutlineUserAdd size={25} />
+            <span className="text-2xl font-semibold pb-1">Sign up</span>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
