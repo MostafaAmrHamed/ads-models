56acf0ac4c20c08b66444a4db2fc5767d4448222
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-03-05 03:36:56 +0200

    feat(dashboard): imported createAd component

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateAd from "./components/CreateAd";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const [createAd, setCreateAd] = useState(false);
+  return (
+    <div className="ml-2 xl:mx-auto mt-5 space-y-5">
+      <button
+        className="bg-primary-1 text-text text-3xl py-2 px-5 rounded-md font-semibold hover:bg-primary-2 transition-all ease-in-out"
+        onClick={() => {
+          setCreateAd(!createAd);
+        }}
+      >
+        {createAd ? <span>Close</span> : <span>Create Ad+</span>}
+      </button>
+      {createAd && <CreateAd />}
+    </div>
+  );
 }
 
 export default Dashboard;
