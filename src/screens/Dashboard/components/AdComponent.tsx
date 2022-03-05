18eae7c8e57e49c79be7a128ed3ad0e2070e4c73
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-03-05 18:03:16 +0200

    feat(dashboard): testing UpdateAd component

--- src/screens/Dashboard/components/AdComponent.tsx
+++ src/screens/Dashboard/components/AdComponent.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import UpdateAd from "./UpdateAd";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { ad } from "../../../types";
+const adData: ad = {
+  id: 1,
+  title: "Nike Football",
+  type: "video",
+  from: "2022-08-05 6:30 AM",
+  to: "2022-07-05 7:00 PM",
+  link: "https://asdhjasdasmas.jpg",
+};
 const AdComponent = () => {
   const [option, setOption] = useState(false);
+  const [update, setUpdate] = useState(false);
   return (
     <div className="ml-2 pb-2 border-b-[1px] border-text space-y-2">
       <ul
@@ -34,21 +45,37 @@ const AdComponent = () => {
           )}
         </li>
       </ul>
+
       {option && (
-        <div className="flex justify-end gap-2">
-          <button
-            type="button"
-            className="text-white bg-mark-1 text-semibold text-lg md:text-xl py-1 px-7 rounded-md"
-          >
-            Delete
-          </button>
-          <button
-            type="button"
-            className="text-white bg-mark-2 text-semibold text-lg md:text-xl py-1 px-7 rounded-md"
-          >
-            Update
-          </button>
-        </div>
+        <>
+          {update && (
+            <UpdateAd
+              id={adData.id}
+              title={adData.title}
+              type={adData.type}
+              from={adData.from}
+              to={adData.to}
+              link={adData.link}
+            />
+          )}
+          <div className="flex justify-end gap-2">
+            <button
+              type="button"
+              className="text-white bg-mark-1 text-semibold text-lg md:text-xl py-1 px-7 rounded-md"
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setUpdate(!update);
+              }}
+              className="text-white bg-mark-2 text-semibold text-lg md:text-xl py-1 px-7 rounded-md"
+            >
+              Update
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
